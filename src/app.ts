92e2d1b3a64ad6f92e3ee82be7f8c83ed4d24fa2
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-06-30 21:43:54 +0600

    feat(prisma): set up multi-schema structure and define User & Profile modelsfeat: complete Full Express configuration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const app: Application = express();
 
@@ -16,7 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany();
+  console.log(user);
   res.send("Hello World");
 });
 
